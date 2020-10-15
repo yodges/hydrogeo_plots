cda9bff38e0b1475fcd58f26c60168137ecd4d50
Michael Hodges <mhodges@intera.com>
2020-10-15 10:35:07 -0700

    updated single plot with title block to plot multiple sizes of text with separate plt.text() calls and bounding them with empty fig.suptitle() bounded box. also created add_title() function

--- single_plot_skeleton.py
+++ single_plot_skeleton.py
@@ -1,20 +1,30 @@
 import matplotlib.pyplot as plt
 
-def main():
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_position([0.11, -.06, 0.85, 0.7])
-    fig.suptitle('Figure X.Y \n'
-                 'This Figure', x=.88, y=.115, fontsize=14, fontweight='bold',
-                 bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
-    x = [x for x in range(100)]
-    y = [x*x for x in range(100)]
-    ax.set_yscale('linear')
-    ax.plot(x,y, label='data')
-    ax.legend(loc='lower left', bbox_to_anchor=(0.38, 1.01), ncol=2,
-              borderaxespad=0, frameon=False)
+
+def add_title(fig):
+    x_guide = 0.88
+    y_guide = .1
+    fig.suptitle('                  \n'
+                 '                 ', x=x_guide, y=y_guide - .01,
+                 bbox={'facecolor': 'none', 'alpha': 0.5, 'pad': 25})
+    plt.text(x=x_guide, y=y_guide, s="My title", fontsize=12, ha="center", fontweight='bold', transform=fig.transFigure)
+    plt.text(x=x_guide, y=y_guide / 2, s="My title in different size", fontsize=8, ha="center",
+             transform=fig.transFigure)
     return fig
 
+
+def plot():
+    fig, ax = plt.subplots(1, 1)
+    ax.set_position([0.11, 0.15, 0.85, -.3])
+    x = [x for x in range(100)]
+    y = [x * x for x in range(100)]
+    add_title(fig)
+    plt.plot(x, y, label='data')
+    plt.legend(loc='lower left', bbox_to_anchor=(0.38, 1.01), ncol=2,
+               borderaxespad=0, frameon=False)
+    return fig
+
+
 if __name__ == '__main__':
-    main()
+    plot()
     plt.show()
