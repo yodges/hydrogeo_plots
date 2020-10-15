7fb0cf7b6d58a40fc792518ab336c98dc8106d2e
Michael Hodges <mhodges@intera.com>
2020-10-14 19:44:52 -0700

    adjusted annotations to plot suptitle in lower right and reposition axes

--- annotations/test1.py
+++ annotations/test1.py
@@ -4,26 +4,24 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax.set_position([0.11, -.09, 0.85, 0.88])
 
-#fig.subplots_adjust(top=10,bottom=5)
 
 # Set titles for the figure and the subplot respectively
-fig.suptitle('bold figure supertitle', x=.8,y=.05,fontsize=14, fontweight='bold')
+fig.suptitle('Figure x.y \n'
+             'this', x=.8, y=.12, fontsize=14, fontweight='bold')
 
 ax.set_title('axes title')
 
 ax.set_xlabel('xlabel')
 ax.set_ylabel('ylabel')
 
+
 # Set both x- and y-axis limits to [0, 10] instead of default [0, 1]
 ax.axis([0, 10, 0, 10])
 
 ax.text(3, 8, 'boxed italics text in data coords', style='italic',
         bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
-
-ax.text(2, 6, r'an equation: $E=mc^2$', fontsize=15)
-
-ax.text(3, 2, 'unicode: Institut für Festkörperphysik')
 
 ax.text(0.95, 0.01, 'colored text in axes coords',
         verticalalignment='bottom', horizontalalignment='right',
@@ -33,5 +31,7 @@ ax.text(0.95, 0.01, 'colored text in axes coords',
 ax.plot([2], [1], 'o')
 ax.annotate('annotate', xy=(2, 1), xytext=(3, 4),
             arrowprops=dict(facecolor='black', shrink=0.05))
+
+ax2 = fig.add_axes([5, 5, 0.2, 0.2])
 
 plt.show()
