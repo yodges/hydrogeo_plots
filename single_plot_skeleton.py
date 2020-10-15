1dbc014c984846d6029b5030f5a99aa89accdd10
Michael Hodges <mhodges@intera.com>
2020-10-14 20:18:16 -0700

    modified yaxis and added legend at top outside axis

--- single_plot_skeleton.py
+++ single_plot_skeleton.py
@@ -7,10 +7,12 @@ def main():
     fig.suptitle('Figure X.Y \n'
                  'This Figure', x=.88, y=.115, fontsize=14, fontweight='bold',
                  bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
-    x = [x for x in range(1000)]
-    y = [x*x for x in range(1000)]
-    ax.plot(x,y)
-
+    x = [x for x in range(100)]
+    y = [x*x for x in range(100)]
+    ax.set_yscale('linear')
+    ax.plot(x,y, label='data')
+    ax.legend(loc='lower left', bbox_to_anchor=(0.38, 1.01), ncol=2,
+              borderaxespad=0, frameon=False)
     return fig
 
 if __name__ == '__main__':
