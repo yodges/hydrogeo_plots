6128b77007f0d4418451d6c6312dce9a144d53e3
Michael Hodges <mhodges@intera.com>
2020-10-15 16:19:33 -0700

    cleaned pycharm comments and reformated

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from hydrographs import hydrograph
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     hydrograph.plot_hydrograph()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
