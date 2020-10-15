256093616c85824f7a12a10270c1fb0d4b07d8cc
Michael Hodges <mhodges@intera.com>
2020-10-15 14:55:14 -0700

    added template plot

--- hydrographs/hydrograph.py
+++ hydrographs/hydrograph.py
@@ -4,17 +4,23 @@ import skeletons.single_plot_skeleton as sps
 
 
 def plot_hydrograph():
-    fig = plt.figure(figsize=(10, 10))
+    fig = plt.figure(figsize=(12, 10))
     lith.plot_section()
     ax0 = plt.Axes(fig, [.075, .2, .7, .70])
-    sps.add_title(x_guide=0.88, y_guide=0.08, title="Figure X.Y",
-                  subtitle="This Figure needs more text", font1=16, font2=12,
+    sps.add_title(x_guide=0.9, y_guide=0.08, title="Figure X.Y",
+                  subtitle="subtitle", font1=16, font2=12,
                   bbox={'facecolor': 'none', 'alpha': 0.5, 'pad': 60})
     fig.add_axes(ax0)
     ax0.set_ylabel('Depth (ft)')
-    pass
-
+    legend_guide = 0.3
+    x = [x for x in range(100)]
+    y = [x * x for x in range(100)]
+    plt.plot(x, y, label='data')
+    y2 = [5 * x for x in range(100)]
+    plt.plot(x, y2, label='data2')
+    plt.legend(loc='lower left', bbox_to_anchor=(legend_guide, 1.01), ncol=2,
+               borderaxespad=0, frameon=False)
+    plt.show()
 
 if __name__ == '__main__':
     plot_hydrograph()
-    plt.show()
